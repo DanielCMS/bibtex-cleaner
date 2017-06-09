6ae3433a5961188c52e2f3082056eec916f6234c
Linqi <linqi@waltznetworks.com>
2017-06-09 14:48:55 -0700

    Add more tests

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -5,7 +5,7 @@ import pytest
 class TeseRules(TestCase):
     def test_enforce_year_rule(self):
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'title': 'A good paper',
             'author': 'Dummies'
         }
@@ -18,7 +18,7 @@ class TeseRules(TestCase):
 
     def test_no_short_title_rule_when_no_title_supplied(self):
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'author': 'Dummies'
         }
 
@@ -26,7 +26,7 @@ class TeseRules(TestCase):
 
     def test_no_short_title_rule(self):
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'title': 'A bed paper',
             'author': 'Dummies'
         }
@@ -34,7 +34,7 @@ class TeseRules(TestCase):
         assert no_short_title_rule.test(dummy_entry)['passed'] 
 
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'title': 'bed paper',
             'author': 'Dummies'
         }
@@ -43,7 +43,7 @@ class TeseRules(TestCase):
 
     def test_no_super_long_title_rule_when_no_title_supplied(self):
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'author': 'Dummies'
         }
 
@@ -51,7 +51,7 @@ class TeseRules(TestCase):
 
     def test_no_super_long_title_rule(self):
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'title': 'A bed paper',
             'author': 'Dummies'
         }
@@ -59,7 +59,7 @@ class TeseRules(TestCase):
         assert no_super_long_title_rule.test(dummy_entry)['passed'] 
 
         dummy_entry = {
-            'year': 1991,
+            'year': '1991',
             'title': 'This is a super super super super super super \
                     super super super super super super super super \
                     super super super super long and boring paper',
@@ -67,3 +67,28 @@ class TeseRules(TestCase):
         }
 
         assert not no_super_long_title_rule.test(dummy_entry)['passed'] 
+
+    def test_ascii_code_rule(self):
+        dummy_entry = {
+            'year': '1992',
+            'title': 'A nice paper',
+            'author': 'Dummies'
+        }
+
+        assert ascii_code_rule.test(dummy_entry)['passed']
+
+        dummy_entry = {
+            'year': '1992',
+            'title': u'A nice paper\ua000\u1111',
+            'author': 'Dummies'
+        }
+
+        assert not ascii_code_rule.test(dummy_entry)['passed']
+
+        dummy_entry = {
+            'year': '1992',
+            'title': u'A nice paper',
+            'author': u'Dummies\ua000'
+        }
+
+        assert not ascii_code_rule.test(dummy_entry)['passed']
